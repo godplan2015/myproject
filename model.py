7f43a7cbc72a986e5b3d4d6336623fd010d3d4ba
paulokla <paulokla654@gmail.com>
2024-10-25 08:03:08 +0100

    solved the registration error

--- model.py
+++ model.py
@@ -1,23 +1,42 @@
 from flask_sqlalchemy import SQLAlchemy
 from myapp import app
 from flask_wtf import FlaskForm
-from wtforms.validators import InputRequired,DataRequired
-from wtforms import SelectFieldBase, StringField, DecimalField, SelectField, PasswordField
+from wtforms.validators import InputRequired, DataRequired, EqualTo, Email
+from wtforms import SelectFieldBase, StringField, DecimalField, SelectField, PasswordField, SubmitField
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 
 db = SQLAlchemy(app)
 
-class Register_form(FlaskForm):
-    username =StringField('username', [InputRequired()])
-    passwor =PasswordField('passwor', [InputRequired()])
-    confirm=PasswordField('confirm', [InputRequired()])
 
-class myform():
-    username = db.Column(db.String(55), primary_key=True)
-    passwor = db.Column(db.Integer,nullable=False)
-    confirm = db.Column(db.Integer,nullable=True)
+# User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = generate_password_hash(password)
     
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+# Registration Form
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password', message='Passwords must match')])
+    submit = SubmitField('Register')
+
+# Login Form (using Username)
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
 
 with app.app_context():
     db.drop_all()
--- views.py
+++ views.py
@@ -1,54 +1,87 @@
-from flask import Blueprint, jsonify, request,render_template,flash,session,redirect,url_for
+from flask import Blueprint, jsonify, request, render_template, flash, session, redirect, url_for
 from myapp import app
-from model import db
-from flask_sqlalchemy import SQLAlchemy
+from model import db, User
 from flask_wtf import FlaskForm
-from model import myform
-from model import Register_form
-api_blueprint = Blueprint('api_blueprint', __name__)
-app.secret_key = 'your_secret_key' 
+from model import RegisterForm, LoginForm, User
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, current_user, login_required, LoginManager
 
+api_blueprint = Blueprint('api_blueprint', __name__)
+app.secret_key = 'your_secret_key'  # Ideally, use environment variables for this.
+
+# Initialize Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)  # Link LoginManager with your app
+login_manager.login_view = 'api_blueprint.login' 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @api_blueprint.route('/')
 @api_blueprint.route('/home')
 def home():
     return render_template("homes.html")
 
+
 @api_blueprint.route('/about')
 def about():
     return render_template("about.html")
 
 
-@api_blueprint.route('/login', methods=['GET','POST'])
+@api_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
-    form = myform()
-    if request.method == 'POST':
-        first_name = request.form.get("first_name")
-        last_name = request.form.get("last_name")
-        password = request.form.get("password")
-        flash("Login successful!")
-        return redirect('home')
-    
-    return render_template('logins.html') 
-      
+    if current_user.is_authenticated:  # Redirect if already logged in
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)  # Optionally, use login_user(user, remember=True) for "remember me"
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))  # Redirect to home or dashboard
+        else:
+            flash('Login failed. Check your email or password.', 'danger')
+
+    return render_template('logins.html', form=form)
 
 @api_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
-    form = myform()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            username = form.username.data
-            passwor = form.passwor.data
-            confirm= form.confirm.data
-            existing_username = user.query.filter_by(username=username).first()
-            if existing_username:
-                flash('This user has been already taken. Try another one.', 'warning')
-                return render_template('logins.html', form=form)
-            user = user(username, passwor, confirm)
-            db.session.add(user)
-            db.session.commit()
-            flash("Registration successful!")
-    return render_template('registers.html',form=form) 
+    form = RegisterForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        
+        # Check if the username already exists
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            flash('This username is already taken. Try another one.', 'warning')
+            return render_template('registers.html', form=form)
+        
+        # Hash the password for security before saving it to the database
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, password=hashed_password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Registration successful!", 'success')
+        return redirect(url_for('api_blueprint.login'))
+
+    return render_template('registers.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.", 'success')
+    return redirect(url_for('api_blueprint.login'))
 
 
 app.register_blueprint(api_blueprint)
